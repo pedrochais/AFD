0836e6ad0c93983fee86e656b7e13080c9918045
Pedro Rocha <pedrorocha.bou@gmail.com>
2022-07-06 17:05:34 -0300

    corrigindo erro na lógica

--- main.py
+++ main.py
@@ -8,21 +8,26 @@ while(True):
     condicao_3 = False
     ocorrencias_1 = 0
 
+    
+
     for i in range(quantidade_caracteres):
-        if(entrada[i] == 'b' and condicao_0 == True and condicao_1 == True and condicao_2 == True and ocorrencias_1 == 2): # 3
+        #print(entrada[i])
+
+        if(entrada[i] == 'b' and condicao_1 == True and condicao_2 == True and ocorrencias_1 == 2): # 3
             #print("3")
             if(i == (quantidade_caracteres-1)):
                 print("Aceita#3")
             condicao_2 = False
 
-        elif(entrada[i] == 'c' and condicao_0 == True and condicao_1 == True and condicao_2 == False and ocorrencias_1 == 2): # 2
+        elif(entrada[i] == 'c' and condicao_1 == True and condicao_2 == False and ocorrencias_1 == 2): # 2
             #print("2")
             if(i == (quantidade_caracteres-1)):
                 print("Rejeita#2")
             condicao_2 = True
 
-        elif(entrada[i] == 'b' and condicao_0 == True and condicao_1 == False): # 1
-            #print("1")
+        elif(entrada[i] == 'b' and condicao_1 == False): # 1
+            print("1")
+            condicao_0 = True # Não pode mais entrar na condicao_0
             
             ocorrencias_1 += 1
             
@@ -34,8 +39,7 @@ while(True):
             elif(i == (quantidade_caracteres-1) and condicao_1 == True):
                 print("Aceita#1")
 
-        elif(entrada[i] == 'a'): # 0
-            condicao_0 = True
+        elif(entrada[i] == 'a' and condicao_0 == False): # 0
             if(i == (quantidade_caracteres-1)):
                 print("Rejeita#0")
 
